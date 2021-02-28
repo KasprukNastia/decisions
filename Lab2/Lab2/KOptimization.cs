8853d77466d9241ffa47e58ed6fef3ab6dc53e57
Настя Каспрук <anastasiyakaspruk17@gmail.com>
2021-02-28 15:29:41 +0200

    added k-optimization

---
+++ Lab2/Lab2/KOptimization.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Lab2
+{
+    public class KOptimization
+    {
+        public HashSet<int> GetK1BestAlternatives(Relation relation)
+        {
+            return GetBestAlternatives(relation, new List<char> { 'I', 'P', 'N' });
+        }
+
+        public HashSet<int> GetK2BestAlternatives(Relation relation)
+        {
+            return GetBestAlternatives(relation, new List<char> { 'P', 'N' });
+        }
+
+        public HashSet<int> GetK3BestAlternatives(Relation relation)
+        {
+            return GetBestAlternatives(relation, new List<char> { 'I', 'P' });
+        }
+
+        public HashSet<int> GetK4BestAlternatives(Relation relation)
+        {
+            return GetBestAlternatives(relation, new List<char> { 'P' });
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private HashSet<int> GetBestAlternatives(
+            Relation relation,
+            List<char> suitableSetsMarkers)
+        {
+            int[][] characteristicSet = new int[relation.Dimension][];
+
+            Dictionary<int, int> verticesRelationsCounter = new Dictionary<int, int>();
+            for (int i = 0; i < relation.Dimension; i++)
+            {
+                characteristicSet[i] = new int[relation.Dimension];
+                verticesRelationsCounter[i] = 0;
+                for (int j = 0; j < relation.Dimension; j++)
+                {
+                    if (suitableSetsMarkers.Contains(relation.Characteristic[i][j]))
+                    {
+                        characteristicSet[i][j] = 1;
+                        verticesRelationsCounter[i]++;
+                    }
+                }
+            }
+
+            Relation helperSetRelation = new Relation(characteristicSet);
+
+            int maxForBetter = verticesRelationsCounter.Values.Max();
+            HashSet<int> bestAlternatives =
+                verticesRelationsCounter.Where(elem => elem.Value == maxForBetter)
+                .Select(elem => elem.Key)
+                .ToHashSet();
+
+            Dictionary<int, HashSet<int>> bestAlternativesLowerSections =
+                bestAlternatives.ToDictionary(elem => elem, elem => helperSetRelation.GetLowerSection(elem));
+
+            HashSet<int> vertexLowerSection;
+            for (int vertex = 0; vertex < helperSetRelation.Dimension; vertex++)
+            {
+                vertexLowerSection = helperSetRelation.GetLowerSection(vertex);
+                foreach (var alternative in bestAlternativesLowerSections)
+                {
+                    if (vertexLowerSection.Except(alternative.Value).Count() > 0)
+                        bestAlternatives.Remove(alternative.Key);
+                }
+            }
+
+            return bestAlternatives;
+        }
+    }
+}
--- Lab2/Lab2/Relation.cs
+++ Lab2/Lab2/Relation.cs
@@ -6,8 +6,38 @@ namespace Lab2
     public class Relation
     {
         public int[][] Connections { get; }
-
         public int Dimension { get; }
+
+
+        private char[][] _characteristic;
+        public char[][] Characteristic
+        {
+            get
+            {
+                if (_characteristic != null)
+                    return _characteristic;
+
+                _characteristic = new char[Dimension][];
+                for(int i = 0; i < Dimension; i++)
+                {
+                    _characteristic[i] = new char[Dimension];
+                    for(int j = i; j < Dimension; j++)
+                    {
+                        if(Connections[i][j] == 1 && Connections[j][i] == 1)
+                            _characteristic[i][j] = _characteristic[j][i] = 'I';
+                        else if(Connections[i][j] == 0 && Connections[j][i] == 0)
+                            _characteristic[i][j] = _characteristic[j][i] = 'N';
+                        else if (Connections[i][j] == 1 && Connections[j][i] == 0)
+                            _characteristic[i][j] = 'P';
+                        else if (Connections[i][j] == 0 && Connections[j][i] == 1)
+                            _characteristic[j][i] = 'P';
+                    }
+                }
+
+                return _characteristic;
+            }
+        }
+
         public Relation(int[][] connections)
         {
             Connections = connections ?? throw new ArgumentNullException(nameof(connections));
@@ -40,6 +70,21 @@ namespace Lab2
             }
 
             return upperSection;
+        }
+
+        public HashSet<int> GetLowerSection(int vertex)
+        {
+            if (vertex < 0 || vertex >= Dimension)
+                throw new ArgumentException($"The vertex {vertex} does not belong to the relation");
+
+            HashSet<int> lowerSection = new HashSet<int>();
+            for (int i = 0; i < Dimension; i++)
+            {
+                if (Connections[vertex][i] == 1)
+                    lowerSection.Add(i);
+            }
+
+            return lowerSection;
         }
     }
 }
